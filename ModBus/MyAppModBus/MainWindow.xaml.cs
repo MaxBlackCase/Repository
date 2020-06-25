d4919997f01aff524b1b0b999a9837173ce44606
Max Black <maxblackcase@gmail.com>
2020-06-25 10:16:41 +0400

    Успешное реализовано подключение к ModBus

--- ModBus/MyAppModBus/MainWindow.xaml.cs
+++ ModBus/MyAppModBus/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using Modbus.Utility;
 using System;
 using System.Collections.Generic;
 using System.IO.Ports;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Windows;
 using System.Windows.Documents;
@@ -65,11 +66,13 @@ namespace MyAppModBus {
           btnGetHoldReg.IsEnabled = false;
         }
         _serialPort.PortName = comboBoxMainPorts.Text;
-        _serialPort.BaudRate = 19200;
+        _serialPort.BaudRate = 119200;
         _serialPort.Parity = Parity.None;
         _serialPort.StopBits = StopBits.One;
+        _serialPort.ReadTimeout = 100;
+        _serialPort.WriteTimeout = 300;
+        _serialPort.DtrEnable = true;
         _serialPort.Open();
-
         timer.Tick += new EventHandler( getHoldReg );
         timer.Interval = new TimeSpan( 0, 0, 1 );
         timer.Start();
@@ -84,7 +87,7 @@ namespace MyAppModBus {
       catch ( Exception ex ) {
         connectComPort.Content = "Подкл";
         _serialPort.Close();
-        textViewer.Text = ex.Message;
+        textViewer.Text += ex.Message;
 
       }
 
@@ -100,58 +103,80 @@ namespace MyAppModBus {
       btnGetHoldReg.IsEnabled = false;
     }
 
+    public void getHoldReg( object sender, EventArgs e ) {
 
-    private void getHoldReg( object sender, EventArgs e ) {
 
-      
-      byte funcCode = 3;
-      ushort startAddr = 0;
-      ushort numOfPoints = 9;
+      try {
+        //byte funcCode = 3;
+        ushort startAddr = 0;
+        ushort numOfPoints = 18;
 
-      //Создание запроса
-      byte[] frame = new byte[ 8 ];
-      frame[ 0 ] = slaveID;
-      frame[ 1 ] = funcCode;
-      frame[ 2 ] = (byte)(startAddr >> 8);
-      frame[ 3 ] = (byte)startAddr;
-      frame[ 4 ] = (byte)(numOfPoints >> 8);
-      frame[ 5 ] = (byte)numOfPoints;
-      byte[] checkSum = CRC16( frame );
-      frame[ 6 ] = checkSum[ 0 ];
-      frame[ 7 ] = checkSum[ 1 ];
+        ModbusSerialMaster master = ModbusSerialMaster.CreateRtu( _serialPort );
 
-      //Вывод в тектовое окно
-      textViewer.Text += "\n\n";
-      foreach ( var item in frame ) {
+        ushort[] result = master.ReadHoldingRegisters( slaveID, startAddr, numOfPoints );
 
-        textViewer.Text += string.Format( "{0:X2} ", item);
+        textViewer.Text = "";
+        int i = 0;
+        foreach ( ushort item in result ) {
 
+          textViewer.Text += $"Регистр: {i} \t{item}\n";
+          
+          i++;
+        }
       }
+      catch ( Exception err ) {
+
+        textViewer.Text = $"Error: {err.Message}";
+      }
+
+
+      #region Контрольная сумма
+      //Создание запроса
+
+      //byte[] frame = new byte[ 8 ];
+      //frame[ 0 ] = slaveID;
+      //frame[ 1 ] = funcCode;
+      //frame[ 2 ] = (byte)(startAddr >> 8);
+      //frame[ 3 ] = (byte)startAddr;
+      //frame[ 4 ] = (byte)(numOfPoints >> 8);
+      //frame[ 5 ] = (byte)numOfPoints;
+      //byte[] checkSum = CRC16( frame );
+      //frame[ 6 ] = checkSum[ 0 ];
+      //frame[ 7 ] = checkSum[ 1 ];
+
+      ////Вывод в тектовое окно
+      //textViewer.Text += "\n\n";
+      //foreach ( var item in frame ) {
+
+      //  textViewer.Text += string.Format( "{0:X2} ", item );
+
+      //}
 
     }
 
     /// <summary>
     /// Подсчет контрольной суммы
     /// </summary>
-    /// <param name="data">Данные массива запроса</param>
+    /// <param name = "data" > Данные массива запроса</param>
     /// <returns></returns>
-    private static byte[] CRC16( byte[] data ) {
+    //private static byte[] CRC16( byte[] data ) {
 
-      byte[] checkSum = new byte[ 2 ];
-      ushort reg_crc = 0xFFFF;
-      for ( int i = 0; i < data.Length; i++ ) {
-        if ( (reg_crc & 0x01) == 1 ) {
-          reg_crc = (ushort)((reg_crc >> 1) ^ 0xA001);
-        }
-        else {
-          reg_crc = (ushort)(reg_crc >> 1);
-        }
-      }
+    //  byte[] checkSum = new byte[ 2 ];
+    //  ushort reg_crc = 0xFFFF;
+    //  for ( int i = 0; i < data.Length; i++ ) {
+    //    if ( (reg_crc & 0x01) == 1 ) {
+    //      reg_crc = (ushort)((reg_crc >> 1) ^ 0xA001);
+    //    }
+    //    else {
+    //      reg_crc = (ushort)(reg_crc >> 1);
+    //    }
+    //  }
 
-      checkSum[ 1 ] = (byte)((reg_crc >> 8) & 0xFF);
-      checkSum[ 0 ] = (byte)(reg_crc & 0xFF);
-      return checkSum;
-    }
+    //  checkSum[ 1 ] = (byte)((reg_crc >> 8) & 0xFF);
+    //  checkSum[ 0 ] = (byte)(reg_crc & 0xFF);
+    //  return checkSum;
+    //}
+    #endregion
 
   }
 }
