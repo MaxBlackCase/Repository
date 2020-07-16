6e2d9fb0560e9700b142587ed63f0232efb3dafd
Max Black <maxblackcase@gmail.com>
2020-07-16 13:04:47 +0400

    Переработка занесения данных на график

--- ModBus/MyAppModBus/MainWindow.xaml.cs
+++ ModBus/MyAppModBus/MainWindow.xaml.cs
@@ -1,16 +1,18 @@
-﻿using Modbus.Device;
+﻿using ControlzEx.Theming;
+using InteractiveDataDisplay.WPF;
+using MahApps.Metro.Controls;
+using Modbus.Device;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO.Ports;
 using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
 using System.Windows.Threading;
-using System.Collections.Generic;
-using InteractiveDataDisplay.WPF;
-using MahApps.Metro.Controls;
-using ControlzEx.Theming;
 
 namespace MyAppModBus {
   /// <summary>
@@ -29,18 +31,13 @@ namespace MyAppModBus {
     public static SerialPort _serialPort = null;
     public static ModbusSerialMaster master = null;
 
-    private Dictionary<int, double> volltage = new Dictionary<int, double>();
-    private Dictionary<int, double> current = new Dictionary<int, double>();
-    private Dictionary<int, double> torque = new Dictionary<int, double>();
-    private Dictionary<int, double> tempExternal = new Dictionary<int, double>();
-    private Dictionary<int, double> tempMotor = new Dictionary<int, double>();
+    private Dictionary<double, double>[][] _arrDict = new Dictionary<double, double>[ 2 ][];
 
-    private LineGraph volltageLine = new LineGraph();
-    private LineGraph currentLine = new LineGraph();
-    private LineGraph torqueLine = new LineGraph();
-    private LineGraph externalLine = new LineGraph();
-    private LineGraph motorLine = new LineGraph();
+    private LineGraph _lines;
+    private LineGraph[,] _linesArr = new LineGraph[ 2,3];
 
+    private string[] nameLinesOne = new string[ 3 ] { "Volltage", "Current", "Torque" };
+    private string[] nameLinesTwo = new string[ 2 ] { "External", "Motor" };
 
     /// <summary>
     /// Главнео окно
@@ -48,8 +45,8 @@ namespace MyAppModBus {
     public MainWindow() {
       InitializeComponent();
       AddItemToComboBox();
-      ThemeManager.Current.ChangeTheme( this, "Dark.Steel" );
-      GraphLines();
+      ThemeManager.Current.ChangeTheme( this, "Dark.Blue" );
+      GraphLines( 1.5, nameLinesOne, nameLinesTwo, _lines );
     }
 
     //Инициализация портов
@@ -157,33 +154,40 @@ namespace MyAppModBus {
 
     private double countTime = 0;
     private int countIndex = 0;
-
+    private int[][] _numberRegisters = new int[ 2 ][];
     private void GetHoldReg( object sender, EventArgs e ) {
       ushort[] result = master.ReadHoldingRegisters( slaveID, startAddress, numburOfPoints );
+      
       try {
 
-        textViewer.Text = "";
+        textViewer.Text = null;
         countTime += readWriteTimeOut;
 
+        ///Вывод всех регистров на экран
         for ( int i = 0; i < result.Length; i++ ) {
           textViewer.Text += $"Регистр: {i} \t{result[ i ]}\n";
 
         }
 
+        ///Запуск функции отображения концевиков
         SetValSingleRegister( result[ 9 ], result[ 10 ] );
 
+        ///Занесение значений на график
         if ( countTime % readWriteTimeOut == 0 ) {
-          volltage.Add( countIndex, Convert.ToDouble( result[ 0 ] ) );
-          current.Add( countIndex, Convert.ToDouble( result[ 1 ] ) );
-          torque.Add( countIndex, Convert.ToDouble( result[ 4 ] ) );
-          tempExternal.Add( countIndex, Convert.ToDouble( result[ 2 ] ) );
-          tempMotor.Add( countIndex, Convert.ToDouble( result[ 3 ] ) );
-
-          volltageLine.Plot( volltage.Keys, volltage.Values );
-          currentLine.Plot( current.Keys, current.Values );
-          torqueLine.Plot( torque.Keys, torque.Values );
-          externalLine.Plot( tempExternal.Keys, tempExternal.Values );
-          motorLine.Plot( tempMotor.Keys, tempMotor.Values );
+          for ( int writeValueArrOne = 0; writeValueArrOne < _linesArr.GetLength( 0 ); writeValueArrOne++ ) {
+            _arrDict[ 0 ][ writeValueArrOne ] = new Dictionary<double, double>();
+            _arrDict[ 0 ][ writeValueArrOne ].Add(countTime, _numberRegisters[0][ writeValueArrOne ] );
+          }
+          for ( int writeValuArrTwo = 0; writeValuArrTwo < _linesArr.GetLength( 0 ); writeValuArrTwo++ ) {
+            _arrDict[ 1 ][ writeValuArrTwo ] = new Dictionary<double, double>();
+            _arrDict[ 1 ][ writeValuArrTwo ].Add( countTime, _numberRegisters[ 1 ][ writeValuArrTwo ] );
+          }
+          for ( int i = 0; i < _linesArr.GetLength( 0 ); i++ ) {
+            _linesArr[ 0, i ].Plot( _arrDict[0][i].Keys, _arrDict[ 0 ][ i ].Values );
+          }
+          for ( int j = 0; j < _linesArr.GetLength( 1 ); j++ ) {
+            _linesArr[ 1, j ].Plot( _arrDict[ 1 ][ j ].Keys, _arrDict[ 1 ][ j ].Values );
+          }
 
         }
 
@@ -340,33 +344,33 @@ namespace MyAppModBus {
     /// <summary>
     /// Отрисовка графиков и их линий
     /// </summary>
-    private void GraphLines() {
+    private void GraphLines( double thickness, string[] nameChartOne, string[] nameChartTwo, LineGraph lines ) {
 
-      lines.Children.Add( volltageLine );
-      lines.Children.Add( currentLine );
-      lines.Children.Add( torqueLine );
-      lines_two.Children.Add( externalLine );
-      lines_two.Children.Add( motorLine );
+      _numberRegisters[ 0 ] = new int[ 3 ] { 0, 1, 4 };
+      _numberRegisters[ 1 ] = new int[ 2 ] { 2, 3 };
+
 
       //Линии первого графика
-      volltageLine.Stroke = new SolidColorBrush( Color.FromRgb( 33, 150, 243 ) );
-      volltageLine.Description = String.Format( $"Voltage" );
-      volltageLine.StrokeThickness = 2;
-      currentLine.Stroke = new SolidColorBrush( Color.FromRgb( 76, 175, 80 ) );
-      currentLine.Description = String.Format( $"Current" );
-      currentLine.StrokeThickness = 2;
-      torqueLine.Stroke = new SolidColorBrush( Color.FromRgb( 251, 140, 0 ) );
-      torqueLine.Description = String.Format( $"Torque" );
-      torqueLine.StrokeThickness = 2;
+      for ( int i = 0; i < nameChartOne.Length; i++ ) {
+        lines = new LineGraph();
+        lines.Description = String.Format( $"{nameChartOne[ i ]}" );
+        lines.StrokeThickness = thickness;
+        lines.Stroke = new SolidColorBrush( Color.FromRgb( (byte)(i * 10), 150, (byte)(i * 10) ) );
+        lines_one.Children.Add( lines );
+        _linesArr[ 0, i ] = lines;
+      }
+
+
 
       //Линии второго графика
-      externalLine.Stroke = new SolidColorBrush( Color.FromRgb( 244, 67, 54 ) );
-      externalLine.Description = String.Format( $"Temp Extermal" );
-      externalLine.StrokeThickness = 2;
+      for ( int j = 0; j < nameChartTwo.Length; j++ ) {
+        lines = new LineGraph();
+        lines.Description = nameChartTwo[ j ];
+        lines.StrokeThickness = thickness;
+        lines_two.Children.Add( lines );
+        _linesArr[ 1, j ] = lines;
+      }
 
-      motorLine.Stroke = new SolidColorBrush( Color.FromRgb( 103, 58, 183 ) );
-      motorLine.Description = String.Format( $"Temp Motor" );
-      motorLine.StrokeThickness = 2;
 
     }
 
@@ -424,11 +428,22 @@ namespace MyAppModBus {
     }
 
 
-    //private void ZoomUpSl_ValueChanged( object sender, RoutedEventArgs e ) {
+    //private void ZoomUpSl_ValueChanged( object sender, MahApps.Metro.Controls.RangeParameterChangedEventArgs e ) {
     //  PlotUp.PlotOriginY = (50 * ZoomUpSl.UpperValue) - 50;
     //  PlotUp.PlotHeight = -(50 * (100 - ZoomUpSl.LowerValue));
     //}
 
 
   }
+
+  public class VisibilityToCheckedConverter : IValueConverter {
+    public object Convert( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture ) {
+      return ((Visibility)value) == Visibility.Visible;
+    }
+
+    public object ConvertBack( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture ) {
+      return ((bool)value) ? Visibility.Visible : Visibility.Collapsed;
+    }
+  }
+
 }
