01621e99373bb2f038bdf76dc2ccc5782b377258
MaxBlackCase <maxblackcase@gmail.com>
2020-08-10 16:19:29 +0400

    реализована иблиотека классов для моделей БД и добавление значений в БД

--- ModBus/Test/Test_Entity/Program.cs
+++ ModBus/Test/Test_Entity/Program.cs
@@ -1,8 +1,41 @@
-﻿namespace Test_Entity {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CodeFirst;
+
+namespace Test_Entity {
   internal class Program {
     static void Main( string[] args ) {
 
+      using( var context = new SampleContext() ) {
 
+        var lgp = new List<LineGroup>()
+        {
+          new LineGroup{NameLine = "Volltage" },
+          new LineGroup{NameLine = "Current" },
+          new LineGroup{NameLine = "Torque" },
+          new LineGroup{NameLine = "External" },
+          new LineGroup{NameLine = "Motor" }
+        };
+
+        context.LinesGroup.AddRange( lgp );
+        context.SaveChanges();
+
+        var lp = new List<LinePoint>();
+        var rand = new Random();
+        foreach( var linePointItem in lgp ) {
+
+          lp.Add( new LinePoint { Time = TimeSpan.FromSeconds(rand.Next(1000, 5000)), Values = rand.Next(21, 512), LineGroup = linePointItem.Id} );
+
+        }
+
+        context.LinePoints.AddRange( lp );
+        context.SaveChanges();
+
+
+        Console.WriteLine("OK");
+        Console.Read();
+      }
 
     }
   }
---
+++ ModBus/Test/Test_Entity/SampleContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using CodeFirst;
+
+namespace Test_Entity {
+  internal class SampleContext : DbContext {
+
+    public SampleContext() :base( "DbConnectionString" ) { }
+
+    public DbSet<LineGroup> LinesGroup { get; set; }
+    public DbSet<LinePoint> LinePoints { get; set; }
+
+  }
+}
